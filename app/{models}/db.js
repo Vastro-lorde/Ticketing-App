78c86175568de20803a167678c08815f82420727
Vastro-lorde <omatsolaseund@gmail.com>
2023-11-19 22:22:44 +0100

    changed db connection

--- app/{models}/db.js
+++ app/{models}/db.js
@@ -1,15 +1,31 @@
 // Database connection function
-import { connect } from 'mongoose';
+import mongoose, { connect } from 'mongoose';
 const DB = process.env.MONGODB_URL
 // 
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
 const mongodb = async ()=> {
  
-    try {
-       await connect(DB);
-        console.log(`Database connection successful!`)    
-    } catch (err) {
-        console.log(err); 
-    }
-    }
+    if (cached.conn) {
+        console.log("--- db connected ---");
+        return cached.conn
+      }
+    
+      if (!cached.promise) {
+        const opts = {
+          bufferCommands: false,
+        }
+    
+        cached.promise = connect(DB, opts).then((mongoose) => {
+          console.log("--- db connected ---")
+          return mongoose
+        })
+      }
+      cached.conn = await cached.promise
+      console.log("--- db connected ---")
+}
     
 export default mongodb;
